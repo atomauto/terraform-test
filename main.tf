3a4be8eb3e941759b849065c624ce06b969986e0
Konstantin <konstantin@atomauto.ru>
2024-04-13 23:36:01 +0400

    first variables

--- main.tf
+++ main.tf
@@ -10,7 +10,9 @@ terraform {
 provider "sbercloud" {
   auth_url = "https://iam.ru-moscow-1.hc.sbercloud.ru/v3" 
   region   = "ru-moscow-1"
-
+  project_name = var.project_name
   access_key = var.access_key
   secret_key = var.secret_key
 }
+
+data "sbercloud_availability_zones" "AZ" {}
