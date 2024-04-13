eb3c5f4c288dc534e8bc56d30fa06bffb9e3a1b3
Konstantin <konstantin@atomauto.ru>
2024-04-13 22:58:20 +0400

    PG as backend

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
 terraform {
   backend "pg" {}
+  required_providers {
+    sbercloud = {
+      source  = "tf.repo.sbc.space/sbercloud-terraform/sbercloud" # Initialize Cloud.ru provider
+      }
+  }
+}
+
+provider "sbercloud" {
+  auth_url = "https://iam.ru-moscow-1.hc.sbercloud.ru/v3" 
+  region   = "ru-moscow-1"
+
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
